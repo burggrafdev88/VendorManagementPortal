a9006afc245736dd867c1736ddbb7959762b87a8
Philip Burggraf <burggrafdev88@gmail.com>
2021-03-02 18:15:46 -0600

    3.2 - add vendor, edit vendor and delete vendor functions are now complete.

--- Vendors/urls.py
+++ Vendors/urls.py
@@ -20,6 +20,8 @@ from . import views
 urlpatterns = [
     path('submit_vif', views.submit_vif, name="submit_vif"),
     path('my_vendors', views.my_vendors, name="my_vendors"),
-    path('add_vendor', views.add_vendor, name="add_vendor"),
-    path('edit_vendor', views.EditVendor.as_view, name="edit_vendor"),
+    # path('add_vendor', views.add_vendor, name="add_vendor"),
+    path('edit_vendor', views.EditVendor.as_view(), name="edit_vendor"),
+    path('add_vendor_ajax', views.AddVendorAjax.as_view(), name="add_vendor_ajax"),
+    path('delete_vendor', views.DeleteVendor.as_view(), name="delete_vendor_ajax")
 ]
--- assets/js_functions.js
+++ assets/js_functions.js
@@ -1,64 +1,59 @@
-const overlay = document.getElementById('overlay');
-
-const openModal = (idName) => {
-    // const modal = document.getElementById(idName);
-    // modal.classList.add('active');
-    // overlay.classList.add('active');
-    console.log('Opened brah.');
-};
-
-const closeModal = (idName) => {
-    const modal = document.getElementById(idName);
-    modal.classList.remove('active');
-    overlay.classList.remove('active');
-};
-
-// $( "#addVendorForm22" )[0].submit(function(){
-//    alert("Add vendor handler called.");
-// });
-
+//jQuery to add vendor through AJAX
 $(document).on('submit', '#addVendorForm', function(){
-    console.log('Handler for add vendor called.');
-    //preventing from page reload and default actions
-    // event.preventDefault();
 
-    //serialize the data for sending the form data.
-    let serializedData = $( this ).serialize();
-    console.log(serializedData);
+    //set variables equal to fields in the form.
+    let first_name = $('#addVendorForm input[name=f_name]').val();
+    let last_name = $('#addVendorForm input[name=l_name]').val();
+    let company = $('#addVendorForm input[name=company]').val();
 
-    //make ajax call
-    $.ajax({
-        type: 'POST',
-        url: "add_vendor",
-        data: serializedData,
-        success: function(response){
-            //on successful creating of object - append newly added vendor to table
-            let instance = JSON.parse(response["instance"]);
-            let fields = instance[0]["fields"];
-            $("#vendorTable tbody").prepend(
-                `<tr>
-                    <td>${fields["f_name"]||""}</td>
-                    <td>${fields["l_name"]||""}</td>
-                    <td>${fields["company"]||""}</td>
-                    <td>${fields["physical_access"]||""}</td>
-                    <td>${fields["logical_access"]||""}</td>
-                    <td>${fields["length_of_service"]||""}</td>
-                </tr>`
-            )
-        },
-        error: function (response) {
-            // alert the error if any error occured
-            alert(response["responseJSON"]["error"]);
-        }
-    });
+    //check if physical access field is checked. Then set equal to 'True' or 'False'. (jQuery sets it equal to
+    // 'true' or 'false'.  Python needs the first letter capitalized to be able to read it.
+    let physical_access = $('#addVendorForm input[name=physical_access]').is(':checked');
+    physical_access = physical_access === true ? 'True' : 'False';
+
+    //check if physical access field is checked. Then set equal to 'True' or 'False' depending on its value.
+    // (jQuery sets it equal to 'true' or 'false'.  Python needs the first letter capitalized to be able to read it.
+    let logical_access = $('#addVendorForm input[name=logical_access]').is(':checked');
+    logical_access = logical_access === true ? 'True' : 'False';
+
+    //check if logical access field is checked. Then set equal to 'True' or 'False' depending on its value.
+    // (jQuery sets it equal to 'true' or 'false'.  Python needs the first letter capitalized to be able to read it.
+    let length_of_service = $('#addVendorForm input[name=length_of_service]').val();
+    let work_description = $('#addVendorForm input[name=work_description]').val();
+
+    let bank_poc = $('#addVendorForm input[name=user_id]').val();
+
+    //if all fields have a value, execute an ajax call.
+    if(first_name && last_name && company && physical_access != null && logical_access != null && length_of_service &&
+    work_description) {
+
+        //make ajax call
+        $.ajax({
+            url: 'add_vendor_ajax',
+            data: {
+                'f_name': first_name,
+                'l_name': last_name,
+                'company': company,
+                'physical_access': physical_access,
+                'logical_access': logical_access,
+                'length_of_service': length_of_service,
+                'work_description': work_description,
+                'bank_poc': bank_poc,
+            },
+            dataType: 'json',
+            success: function (data) {
+                if (data.vendor) {
+                    alert("Your vendor has been added.");
+                }
+            },
+            error: function (error) {
+                alert('There was an error. Please try again. ' + error);
+            }
+        });
+    }
 });
 
-
-$(document).on('click', '#editVendorButton', function () {
-    let id = $(this).attr('id');
-    alert("Howdy! " + id);
-});
-
+/*JavaScript function to pull up edit vendor form*/
 const editVendorFunction = (id) => {
     let tr_id = "#vendor" + id;
     let first_name = $(tr_id).find(".first_name").text();
@@ -69,6 +64,7 @@ const editVendorFunction = (id) => {
     let length_of_service = $(tr_id).find(".length_of_service").text();
     let work_description = $(tr_id).find(".work_description").text();
 
+    $('#editVendor input[name=vendor_id]').val(id);
     $('#editVendor input[name=f_name]').val(first_name);
     $('#editVendor input[name=l_name]').val(last_name);
     $('#editVendor input[name=company]').val(company);
@@ -92,67 +88,90 @@ const editVendorFunction = (id) => {
     $('#editVendor input').prop('disabled', true);
 }
 
+/*JavaScript to change edit vendor form from readonly to editable*/
 const enableForm = () => {
     $('#editVendor input').prop('readonly', false);
     $('#editVendor input').prop('disabled', false);
 }
 
-// Update Django AJAX call.
+// Django AJAX call to update record.
 $(document).on('submit', '#editVendor', function() {
+    let vendor_id = $('#editVendor input[name="vendor_id"]').val();
     let first_name = $('#editVendor input[name="f_name"]').val();
     let last_name = $('#editVendor input[name="l_name"]').val();
     let company = $('#editVendor input[name="company"]').val();
+
+    //check if physical access field is checked. Then set equal to 'True' or 'False' depending on its value.
+    // (jQuery sets it equal to 'true' or 'false'.  Python needs the first letter capitalized to be able to read it.
     let physical_access = $('#editVendor input[name="physical_access"]').is(":checked");
+    physical_access = physical_access === true ? 'True' : 'False';
+
+    //check if logical access field is checked. Then set equal to 'True' or 'False' depending on its value.
+    // (jQuery sets it equal to 'true' or 'false'.  Python needs the first letter capitalized to be able to read it.
     let logical_access = $('#editVendor input[name="logical_access"]').is(":checked");
+    logical_access = logical_access === true ? 'True' : 'False';
+
     let length_of_service = $('#editVendor input[name="length_of_service"]').val();
     let work_description = $('#editVendor input[name="work_description"]').val();
 
-    alert('Test ajax function.');
+    if (vendor_id && first_name && last_name && company && physical_access != null && logical_access != null && length_of_service &&
+        work_description) {
 
-    // var idInput = $('input[name="formId"]').val().trim();
-    // var nameInput = $('input[name="formName"]').val().trim();
-    // var addressInput = $('input[name="formAddress"]').val().trim();
-    // var ageInput = $('input[name="formAge"]').val().trim();
-    // if (nameInput && addressInput && ageInput) {
-    //     // Create Ajax Call
-    //     $.ajax({
-    //         url: '{% url "crud_ajax_update" %}',
-    //         data: {
-    //             'id': idInput,
-    //             'name': nameInput,
-    //             'address': addressInput,
-    //             'age': ageInput
-    //         },
-    //         dataType: 'json',
-    //         success: function (data) {
-    //             if (data.user) {
-    //                 updateToUserTabel(data.user);
-    //             }
-    //         }
-    //     });
-    // } else {
-    //     alert("All fields must have a valid value.");
-    // }
-    // $('form#updateUser').trigger("reset");
-    // $('#myModal').modal('hide');
-    // return false;
+        // Create ajax call
+        $.ajax({
+            url: 'edit_vendor',
+            data: {
+                'id': vendor_id,
+                'f_name': first_name,
+                'l_name': last_name,
+                'company': company,
+                'physical_access': physical_access,
+                'logical_access': logical_access,
+                'length_of_service': length_of_service,
+                'work_description': work_description,
+            },
+            dataType: 'json',
+            success: function (data) {
+                if (data) {
+                    alert("Vendor data has been updated.");
+                }
+            },
+            error: function (error) {
+                alert(error);
+            }
+        });
+    //    if not all fields are filled in in the if statement above, alert the message below.
+    } else {
+        alert("All fields must be valid.");
+    }
 });
 
-// Update Django Ajax Call
-function updateUser(id) {
-    if (id) {
+/*JavaScript to delete vendor record.*/
+const deleteVendorFunction = (id) =>{
+    let vendor_id = id;
 
-
-
-        // name = $(tr_id).find(".userName").text();
-        // address = $(tr_id).find(".userAddress").text();
-        // age = $(tr_id).find(".userAge").text();
-        // $('#form-id').val(id);
-        // $('#form-name').val(name);
-        // $('#form-address').val(address);
-        // $('#form-age').val(age);
-    }
+    // Create ajax call
+    $.ajax({
+        url: 'delete_vendor',
+        data: {
+            'id': vendor_id,
+        },
+        dataType: 'json',
+        success: function (status) {
+            if (status) {
+                alert("Vendor data has been deleted.");
+                location.reload();
+            } else{
+                alert("Vendor delete was unsuccessful");
+            }
+        },
+        error: function (error) {
+            alert(error);
+        }
+    });
 }
+
+
 
 
 
